e99d1181d6da60494096b3036b3e93550623a360
ridwanmafhumy <ridwanmafhumy@gmail.com>
2021-02-11 18:04:50 +0700

    add template whats new dan event detail

---
+++ event-detail.php
@@ -0,0 +1,58 @@
+<?php include "header.php"; ?>
+<section id="hero-image">
+  <div id="hero-slider" class="owl-carousel">
+    <div class="event-detail-banner">
+      <img src="./assets/images/banner-event2.jpg" alt="">
+      <div class="page-title">
+        event name
+        <p>dd-mm-yyyy</p>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="content-wrapper">
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-4 no-padding ">
+        <div class="card-item ">
+          <div class="card-image">
+            <img src="./assets/images/dummy-post3.jpg" alt="">
+          </div>
+        </div>
+      </div>
+      <div class="col-md-8 no-padding align-self-center">
+        <div class="card-item">
+          <div class="desc-image">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non veritatis fugiat quaerat magnam totam eligendi reprehenderit ratione, excepturi, vero a quas numquam beatae voluptatem dicta laudantium pariatur obcaecati unde illum.
+            <br>
+            <br>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim, delectus id saepe deserunt voluptas laborum soluta modi itaque nulla dolores laboriosam atque? Eveniet repellat reprehenderit hic quisquam accusamus, cumque similique.
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row justify-content-center">
+      <div class="col-md-4 no-padding">
+        <div class="card-item">
+          <div class="card-image">
+            <img src="./assets/images/dummy-post2.jpg" alt="">
+          </div>
+        </div>
+      </div>
+      <div class="col-md-8 no-padding ">
+        <div class="card-item">
+          <div class="card-image">
+            <div class="embed-responsive embed-responsive-21by9">
+              <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/tv7Hyz4hqm0"></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</section>
+<section class="content-padding">
+  <?php include "footer.php"; ?>
+  
---
+++ whats-new.php
@@ -0,0 +1,82 @@
+<?php include "header.php"; ?>
+<section id="hero-image">
+  <div id="hero-slider" class="owl-carousel">
+    <div class="event-detail-banner">
+      <img src="./assets/images/banner-event2.jpg" alt="">
+      <div class="page-title">
+        what's new
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="content-wrapper">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+        <div class="card-item">
+          <div class="card-image">
+            <img src="./assets/images/dummy-post3.jpg" alt="">
+            <div class="card-title">
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card-item">
+          <div class="card-image">
+            <img src="./assets/images/dummy-post3.jpg" alt="">
+            <div class="card-title">
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card-item">
+          <div class="card-image">
+            <img src="./assets/images/dummy-post3.jpg" alt="">
+            <div class="card-title">
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-4">
+        <div class="card-item">
+          <div class="card-image">
+            <img src="./assets/images/dummy-post3.jpg" alt="">
+            <div class="card-title">
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card-item">
+          <div class="card-image">
+            <img src="./assets/images/dummy-post3.jpg" alt="">
+            <div class="card-title">
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card-item">
+          <div class="card-image">
+            <img src="./assets/images/dummy-post3.jpg" alt="">
+            <div class="card-title">
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php include "footer.php"; ?>
