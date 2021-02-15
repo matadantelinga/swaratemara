96bb4dfd9a7ef74574ede92aef2101dc34014b92
Matadantelinga <macbookpro@192.168.1.5>
2021-02-15 21:19:27 +0700

    video home

--- index.php
+++ index.php
@@ -9,7 +9,33 @@
     </div>
   </div>
 </section>
-<section class="content-padding">
-
+<section id="home-video" class="content-padding" style="background: url(./assets/images/homevideo-bg.jpg);">
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-10">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="video-holder">
+              <img src="./assets/images/dummy-video.jpg" alt="">
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-3">
+            <div class="text-big">
+              SWARA IDEAS
+            </div>
+          </div>
+          <div class="col-md-9">
+            <div class="text-title">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris a
+              tortor vestibulum, accumsan ligula ut, lobortis nisl.
+              Curabitur sit amet mollis tellus vel, ornare massa. 
+            </div>
+        </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </section>
 <?php include "footer.php"; ?>
