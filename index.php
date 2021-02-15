a35260d480e932b94f46a7c1ee6b062ea27dd202
Matadantelinga <macbookpro@192.168.1.5>
2021-02-16 00:01:18 +0700

    homepage

--- index.php
+++ index.php
@@ -38,4 +38,156 @@
     </div>
   </div>
 </section>
+
+<section id="home-services" style="background: url(./assets/images/banner-event2.jpg) center no-repeat;">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="section-title">
+          OUR SERVICES
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-3">
+        <div class="services-item">
+          <div class="services-image">
+            <img src="./assets/images/icon-hand.svg" alt="">
+            <div class="services-title">
+              MICE
+            </div>
+            <a href="#" class="btn-line-gray">DETAIL</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="services-item">
+          <div class="services-image">
+            <img src="./assets/images/icon-people.svg" alt="">
+            <div class="services-title">
+              BRAND MANAGEMENT
+            </div>
+            <a href="#" class="btn-line-gray">DETAIL</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="services-item">
+          <div class="services-image">
+            <img src="./assets/images/icon-speaker.svg" alt="">
+            <div class="services-title">
+              EVENT MANAGEMENT
+            </div>
+            <a href="#" class="btn-line-gray">DETAIL</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="services-item">
+          <div class="services-image">
+            <img src="./assets/images/icon-graph.svg" alt="">
+            <div class="services-title">
+              DIGITAL MARKETING
+            </div>
+            <a href="#" class="btn-line-gray">DETAIL</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="post-image-container content-wrapper">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="section-title text-center">
+          WHAT'S NEW
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-4">
+        <div class="post-image-item">
+          <div class="post-image" style="background: url(./assets/images/dummy-post.jpg);">
+          </div>
+          <div class="post-title">
+            DUMMY TITLE
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="post-image-item">
+          <div class="post-image" style="background: url(./assets/images/dummy-post2.jpg);">
+          </div>
+          <div class="post-title">
+            DUMMY TITLE
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="post-image-item">
+          <div class="post-image" style="background: url(./assets/images/dummy-post3.jpg);">
+          </div>
+          <div class="post-title">
+            DUMMY TITLE
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="post-image-container content-wrapper">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="section-title text-center">
+          UPCOMING EVENT
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-4">
+        <div class="post-image-item">
+          <div class="post-image" style="background: url(./assets/images/dummy-post3.jpg);">
+          </div>
+          <div class="post-title">
+            DUMMY TITLE
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="post-image-item">
+          <div class="post-image" style="background: url(./assets/images/dummy-post2.jpg);">
+          </div>
+          <div class="post-title">
+            DUMMY TITLE 
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="post-image-item">
+          <div class="post-image" style="background: url(./assets/images/dummy-post.jpg);">
+          </div>
+          <div class="post-title">
+            DUMMY TITLE
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section id="ig-feed">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="section-title">
+          FOLLOW US ON INSTAGRAM
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 <?php include "footer.php"; ?>
